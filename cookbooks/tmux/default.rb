189db43dabc0030db181c2d089518d7beca4337b
masaki.arizuka <masaki.arizuka@gmail.com>
2020-01-04 00:42:53 +0900

    Simplify tmux.conf

--- cookbooks/tmux/default.rb
+++ cookbooks/tmux/default.rb
@@ -5,3 +5,7 @@ dotfile 'tmux.conf'
 file "#{ENV['HOME']}/.tmux.conf.local" do 
     owner node[:user]
 end
+
+git "#{ENV['HOME']}/.tmux/plugins/tpm" do
+    repository "https://github.com/tmux-plugins/tpm"
+end
