55a0dd60095f35e6bebd44fb535219dc6075d536
masaki.arizuka <masaki.arizuka@gmail.com>
2020-01-05 04:21:38 +0900

    rubocop -a

--- roles/darwin.rb
+++ roles/darwin.rb
@@ -1,30 +1,30 @@
 include_cookbook 'base'
 
-include_cookbook "homebrew"
+include_cookbook 'homebrew'
 
-include_cookbook "z"
-include_cookbook "zsh"
+include_cookbook 'z'
+include_cookbook 'zsh'
 
-include_cookbook "anyenv"
-include_cookbook "direnv"
-include_cookbook "ghq"
-include_cookbook "git"
-include_cookbook "karabiner"
-include_cookbook "poetry"
-include_cookbook "tmux"
-include_cookbook "vim"
-include_cookbook "vscode"
+include_cookbook 'anyenv'
+include_cookbook 'direnv'
+include_cookbook 'ghq'
+include_cookbook 'git'
+include_cookbook 'karabiner'
+include_cookbook 'poetry'
+include_cookbook 'tmux'
+include_cookbook 'vim'
+include_cookbook 'vscode'
 
-package "bat"
-package "coreutils"
-package "gnu-sed"
-package "htop-osx"
-package "httpie"
-package "jq"
-package "nkf"
-package "the_silver_searcher"
-package "tfenv"
-package "tig"
-package "tree"
-package "watch"
-package "wget"
+package 'bat'
+package 'coreutils'
+package 'gnu-sed'
+package 'htop-osx'
+package 'httpie'
+package 'jq'
+package 'nkf'
+package 'the_silver_searcher'
+package 'tfenv'
+package 'tig'
+package 'tree'
+package 'watch'
+package 'wget'
