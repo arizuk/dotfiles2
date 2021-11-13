98969bc65e6aba1a999ce0b52a6e79f754bddf0f
masaki.arizuka <masaki.arizuka@gmail.com>
2021-11-13 21:05:49 +0900

    cpuのarch依存のコードを変更

--- cookbooks/ghq/default.rb
+++ cookbooks/ghq/default.rb
@@ -1,6 +1,11 @@
-github_binary 'ghq' do
-  repository 'motemen/ghq'
-  version 'v0.10.0'
-  archive "ghq_#{node[:os]}_amd64.zip"
-  binary_path "ghq_#{node[:os]}_amd64/ghq"
+cpu_arch = run_command('uname -m')
+if cpu_arch == 'x86_64'
+  github_binary 'ghq' do
+    repository 'motemen/ghq'
+    version 'v0.10.0'
+    archive "ghq_#{node[:os]}_amd64.zip"
+    binary_path "ghq_#{node[:os]}_amd64/ghq"
+  end
+else
+  raise NotImplementedError
 end
