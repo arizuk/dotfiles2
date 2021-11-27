9ddc388a5d37ab1fb9bc68a6ef3bb36bc022c15f
masaki.arizuka <masaki.arizuka@gmail.com>
2021-11-27 21:49:59 +0900

    Revert "Remove --tac option"
    This reverts commit 3da851952f337b529bbf5679dc29ff0d6ab5a35f.

--- dotfiles/zshrc.d/sk.zsh
+++ dotfiles/zshrc.d/sk.zsh
@@ -1,6 +1,6 @@
 function sk-select-history()
 {
-    BUFFER=$(fc -l -n 1 | sk --query "$LBUFFER")
+    BUFFER=$(fc -l -n 1 | sk --tac --query "$LBUFFER")
     CURSOR=$#BUFFER             # move cursor
     zle -R -c                   # refresh
 }
